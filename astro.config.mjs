d730819e7d5060d97012a1a1072120776cd7402f
iapplus <ipasswd@163.com>
2026-04-13 17:51:07 +0800

    !

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@
 import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 import vue from '@astrojs/vue';
-import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel(),
     integrations: [
         starlight({
             title: 'WELOCK',
